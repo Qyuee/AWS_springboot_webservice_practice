3b01d8ef5fd24ce71ce7778744e913eef4016213
Qyuee <lee33398@naver.com>
2021-05-09 23:31:38 +0900

    Initial commit

---
+++ src/test/java/com/qyuee/book/AWS_springboot_webservice_practice/AwsSpringbootWebservicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qyuee.book.AWS_springboot_webservice_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootWebservicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
